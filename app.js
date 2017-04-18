1a0c100f58fc9ecf5ee37394325adc4862dea6ec
Miguel <grimor_666@hotmail.com>
2017-04-18 10:19:45 +0200

    cambio con git desktop
    cambios con gitdesktop

--- app.js
+++ app.js
@@ -7,12 +7,25 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const users = require('./routes/users');
+
+//port number
 const port = 3000;
 
+//CORS Middleware
+app.use(cors());
+
+//body parser middleware
+app.use(bodyParser.json());
+
+app.use('/users',users);
+
+//Index route
 app.get('/', (req,res) =>{
     res.send('Invalid Endpoint');
 });
 
+//Start server
 app.listen(port, ()=>{
     console.log('Server started in '+port);
 });
