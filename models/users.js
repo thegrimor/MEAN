b616eefe52dd54ac28b7739581f2d271f1f294da
Miguel <grimor_666@hotmail.com>
2017-04-19 17:58:31 +0200

    Angular 2.0 instalacion

--- models/users.js
+++ models/users.js
@@ -28,7 +28,7 @@ module.exports.getUserById = function(id,callback){
 }
 
 module.exports.getUserByUsername = function(username,callback){
-    const query = {username:username}; 
+    const query = {username:username};   
   User.findOne(query,callback);
 }
 
